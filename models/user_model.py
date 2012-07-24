605b14e9be3320b334a4e1630d4d58cd1db510fb
chalupaul <paul@paulsresume.net>
2012-07-24 12:33:28 -0500

    changing some things around so that the submodules are a little more sane

--- models/user_model.py
+++ models/user_model.py
@@ -1,4 +1,7 @@
-class User(Base):
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+class User(declarative_base()):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String)
--- windfall/Windfall.py
+++ windfall/Windfall.py
@@ -1,9 +1,19 @@
 import os, sys, re, yaml, bottle
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 app_base = os.path.dirname(os.path.realpath(__file__))
 app_base_join = lambda x: os.path.join(os.sep, app_base, x)
 
 configs = yaml.load(open(app_base_join('conf' + os.sep + "application.yaml")))
+engine = create_engine(configs['database'][configs['environment']]\
+                           ['conn_string'], echo=True)
+
+if configs['environment'] is 'dev':
+    declarative_base().metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
 
 bottle.TEMPLATE_PATH.append(app_base_join('views'))
 

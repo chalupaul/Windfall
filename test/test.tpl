a732c8120e632234dbe7bae9a8d47b8d56c0e44a
chalupaul <paul@paulsresume.net>
2012-07-24 11:12:40 -0500

    some virtualenv ended up in there, cleaned it out.

--- test/test.tpl
+++
@@ -1,3 +0,0 @@
-<html>
-<body><h2>Hello there, {{name}}!</h2></body>
-</html>
--- test_controller.py
+++
@@ -1,6 +0,0 @@
-from bottle import route, view
-@route('/test')
-@view('test/test')
-def test():
-    return {"name": "Paul"}
-

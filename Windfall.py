e6ace55fcf3fc71f05eb570cb3a3070f445ee1ee
john5223 <john.curran@rackspace.com>
2012-11-07 15:15:26 -0600

    Better Windfall.py

--- Windfall.py
+++ Windfall.py
@@ -2,22 +2,19 @@ import os, sys, re, yaml, bottle
 from bottle.ext.sqlalchemy import SQLAlchemyPlugin
 from sqlalchemy import create_engine, Column, Integer, Sequence, String
 from sqlalchemy.ext.declarative import declarative_base
-import globals
 
 load_dir = os.path.dirname(os.path.realpath(__file__))
-
 sys.path = [load_dir] + sys.path
 os.chdir(load_dir)
+import globals
 
 
 app_base = os.path.dirname(os.path.realpath(__file__))
 app_base_join = lambda x: os.path.join(os.sep, app_base, x)
 
-
 #Bottle template path and config
 bottle.TEMPLATE_PATH.append(app_base_join('views'))
 configs = yaml.load(open(app_base_join('conf' + os.sep + "application.yaml")))
-
 
 #SQL Alchemy
 engine = create_engine(configs['database'][configs['environment']]['conn_string'], echo=False)
@@ -41,14 +38,16 @@ bottle.install(SQLAlchemyPlugin(engine, globals.Base.metadata, create=True))
 
 
 #Get host of api server from config
-globals.API_SERVER_IP = configs['api_server'][configs['environment']]['host'] + ":" + configs['api_server'][configs['environment']]['port']
+host = configs['api_server'][configs['environment']]['host']
+port = configs['api_server'][configs['environment']]['port']
 
-   
+globals.API_SERVER_IP = host + ":" + port
+
     
 if __name__ == '__main__':
     bottle.debug(True)
     #bottle.run(reloader=True)
-    bottle.run(host=configs['api_server'][configs['environment']]['host'], port=configs['api_server'][configs['environment']]['port'])
+    bottle.run(host=host, port=port)
 else:
      # Mod WSGI launch
     print "MOD WSGI"
